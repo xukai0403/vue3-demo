c7e21a1ab767bb3a5af3ad96649ddb9ba3d5ca16
rick.xukai <rick.xukai@gmail.com>
2024-06-06 21:00:37 +0800

    fix json file error

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import Antd from 'ant-design-vue';
+import App from './App.vue';
+import router from './router';
 
-const pinia = createPinia()
-const app = createApp(App)
+const pinia = createPinia();
+const app = createApp(App);
 
-app.use(pinia)
-app.use(router)
+app.use(pinia);
+app.use(router);
+app.use(Antd);
 
-app.mount('#app')
+app.mount('#app');

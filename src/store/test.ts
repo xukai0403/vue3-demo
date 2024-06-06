d01363c35d48a183312051da525ecc7da33ec709
rick.xu <rick.xu@imaginato.com>
2024-06-06 16:03:56 +0800

    initial

---
+++ src/store/test.ts
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+interface State {
+  count: number
+  testString: string
+}
+
+export const useTestStore = defineStore('test', {
+  state: (): State => {
+    return {
+      count: 0,
+      testString: 'test'
+    }
+  },
+  actions: {
+    setCountNumber() {
+      this.count = this.count + 1
+    }
+  }
+})
---
+++ src/views/TestView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+import { useTestStore } from '@/store/test';
+
+const testStore = useTestStore();
+const { count, setCountNumber } = testStore;
+
+onMounted(() => {
+  console.log('test')
+})
+</script>
+
+<template>
+  <div>123</div>
+  <div>{{ testStore.count }}</div>
+  <button @click="setCountNumber">
+    set count number
+  </button>
+</template>
